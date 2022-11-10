bc35a9d66fdfd3411d044db31431b4c551cb5802
Anderson-Schi <jr93@hotmai.com>
2022-11-10 22:58:08 +0000

    Exercicios resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,26 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+function dados(){
+    let idade = 18;
+    let ensinoMedio = true;
+    let isNotCursandoOutraFaculdade = false;
 
-if(){
+    if(idade >= 18){
+        console.log("A pessoa e maior de idade");
+    }else{
+        console.log("A pessoa e menor de idade");
+    };
+    if(ensinoMedio = true){
+        console.log("A pessoa terminou o ensino medio");
+    }else{
+        console.log("A pessoa nao terminou o ensino medio");
+    };
+    if(isNotCursandoOutraFaculdade = true){
+        console.log("A pessoa nao esta cursando uma faculdade");
+    }else{
+        console.log("A pessoa esta cursando outra faculdade");
+    };
+    
+};
 
-}else{
-	
-}
+dados();
+
+
