c237359f68de6862f7284aa8497c2be052affa00
Anderson-Schi <jr93@hotmai.com>
2022-11-11 20:29:03 +0000

    Exercicio 1 revisado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,16 +1,18 @@
 
 
-function verifica(){
-    const num = 5
+function verifica(num){
+    //const num = 5
     if (num % 2 === 0){
         //console.log("O numero e par");
         return "O numero e par"
+    }else{
+        return "O numero e impar"
     };
     //return "O numero e par"
     
 };
 
-console.log(verifica());
+console.log(verifica(9));
 
 
 
